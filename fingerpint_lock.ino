082fd6449624e38aaa8493c5d757cea009bc76fd
alisonzq <79382590+alisonzq@users.noreply.github.com>
2023-02-15 21:15:46 -0500

    Update fingerpint_lock.ino

--- fingerpint_lock.ino
+++ fingerpint_lock.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_Fingerprint.h>
 #include "SR04.h"
+#include <Servo.h>
 
 #define TRANSISTOR_PIN 5
 #define RELAY_PIN 4
@@ -7,11 +8,11 @@
 #define ECHO_PIN1 12
 #define TRIG_PIN2 11
 #define ECHO_PIN2 10
-#define ACCESS_DELAY 3000 //nombre de temps déverouillé
+#define ACCESS_DELAY 3000 //nombre de temps que la serrure est déverouillé
 
-SoftwareSerial mySerial(2,3);
+SoftwareSerial mySerial(2,3); //branches 2 et 3 pour le capteur fingerprint
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
-
+Servo servo;
 SR04 ultra1 = SR04(ECHO_PIN,TRIG_PIN); //ultrason qui active le capteur fingerprint
 SR04 ultra2 = SR04(ECHO_PIN,TRIG_PIN); //ultrason en arrière de la porte
 
@@ -20,6 +21,7 @@ boolean sensorTurnedOn = false;
 
 void setup() {
   Serial.begin(9600);
+  servo.attach(9); //servo pin 9
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH); //éteint le relay initialement
 }
@@ -50,8 +52,10 @@ void loop() {
   if (getFingerprintID() != -1 || d2 < 10) //si fingerprint match OU la distance du 2e ultrason est moins de 10, le verrou s'ouvre
   {
     digitalWrite(RELAY_PIN, LOW);
+    servo.write(90); //et le servo s'active et la porte s'ouvre de 90 degré
     delay(ACCESS_DELAY);
-    digitalWrite(RELAY_PIN, HIGH); 
+    servo.write(0); //après un nombre de temps "ACCESS_DELAY", la porte ferme à 0 degré
+    digitalWrite(RELAY_PIN, HIGH); //et la serrure se verrouille 
   }  
   delay(50);
 }
@@ -64,10 +68,10 @@ uint8_t getFingerprintID() { //fonction qui vérifie si le fingerprint est dans 
   if (p != FINGERPRINT_OK)  return p;
   p = finger.fingerFastSearch();
 
-  if (p == FINGERPRINT_OK) {
+  if (p == FINGERPRINT_OK) { 
     Serial.println("Found a print match!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) { 
+    Serial.println("Communication error"); //tout ceci permettra de faire des tests, de savoir ce qui se produit lorsque le capteur scan un doigt
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
     Serial.println("Did not find a match");
